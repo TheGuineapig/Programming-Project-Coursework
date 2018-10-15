e75bc33f54c5822923e53a48ee6dc8767433dece
TheGuineapig <niblet321@gmail.com>
2018-10-15 16:42:32 +0100

    added files

---
+++ Game/Assets/scripts/Rotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float angle = 15f;
+    public Vector3 axis = Vector3.up;
+    
+    void Update()
+    {
+        transform.Rotate(axis, angle);
+    }
+}
